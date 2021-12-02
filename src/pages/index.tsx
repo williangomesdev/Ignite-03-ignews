d62956ef288c58e6055e36a83a019917ad62c3ed
Willian Amaro Gomes <willianamaroti@gmail.com>
2021-12-02 01:05:09 -0300

    create src folder

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import styles from '../styles/home.module.scss'
+
+const Home: NextPage = () => {
+  return <h1 className={styles.title}>Hello World!!!</h1>;
+};
+
+export default Home;
