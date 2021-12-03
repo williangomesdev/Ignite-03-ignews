376bdb38c5b87983e8fc616217a39bff953b74d9
Willian Amaro Gomes <willianamaroti@gmail.com>
2021-12-03 12:39:09 -0300

    component Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import styles from "./style.module.scss";
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="Ig.news" />
+        <nav>
+          <a className={styles.active}>Home</a>
+          <a >Posts</a>
+        </nav>
+      </div>
+    </header>
+  );
+}
